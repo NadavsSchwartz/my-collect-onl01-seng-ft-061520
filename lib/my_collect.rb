6a98cb53f94497d801d7ae60ad40e0d10ff56f1b
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-23 14:54:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   i = 0
   while i < collection.length
-    yielded_collection = yieyld(collection[i])
+    yielded_collection = yield(collection[i])
     i += 1
   end
   yielded_collection
